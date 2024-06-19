489662e44d4fc664f15b193ab50701579647fa5b
AbsintheSung <h90243768@gmail>
2024-06-19 16:12:29 +0800

    feat:data bind v-model

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <script setup>
 import TheLayout from './components/TheLayout.vue'
 import TheCard from './components/Cards/TheCard.vue'
+import { ref } from 'vue'
+const userData = ref({
+  tickName: '', //套票名稱
+  tickImg: '', //套票圖片
+  tickArea: '', //區域
+  tickAmount: '', //套票金額
+  tickCount: '', //套票數量
+  tickStar: '', //套票星級
+  tickDescript: '' //套票描述
+})
+const addTicket = () => {
+  console.log(userData.value)
+}
 </script>
 
 <template>
@@ -13,17 +26,17 @@ import TheCard from './components/Cards/TheCard.vue'
       <form class="customFormView">
         <div class="flex flex-wrap">
           <label class="customLabel" for="packageName">套票名稱</label>
-          <input class="customInput" id="packageName" />
+          <input class="customInput" id="packageName" v-model="userData.tickName" />
           <!-- <p class="mt-4 w-full">我是警告標語</p> -->
         </div>
         <div class="flex flex-wrap">
           <label class="customLabel" for="imgUrlName">圖片網址</label>
-          <input class="customInput" id="imgUrlName" />
+          <input class="customInput" id="imgUrlName" v-model="userData.tickImg" />
           <!-- <p class="w-full">我是警告標語</p> -->
         </div>
         <div class="flex flex-wrap">
           <label class="customLabel" for="attractionAreas">景點地區</label>
-          <select class="customInput" id="attractionAreas">
+          <select class="customInput" id="attractionAreas" v-model="userData.tickArea">
             <option value="" disabled selected>請選擇景點地區</option>
             <option>高雄</option>
             <option>台中</option>
@@ -33,25 +46,39 @@ import TheCard from './components/Cards/TheCard.vue'
         </div>
         <div class="flex flex-wrap">
           <label class="customLabel" for="packageAmount">套票金額</label>
-          <input class="customInput" type="number" id="packageAmount" />
+          <input
+            class="customInput"
+            type="number"
+            id="packageAmount"
+            v-model="userData.tickAmount"
+          />
           <!-- <p class="w-full">我是警告標語</p> -->
         </div>
         <div class="flex flex-wrap">
           <label class="customLabel" for="packageTickets">套票組數</label>
-          <input class="customInput" type="number" id="packageTickets" />
+          <input
+            class="customInput"
+            type="number"
+            id="packageTickets"
+            v-model="userData.tickCount"
+          />
           <!-- <p class="w-full">我是警告標語</p> -->
         </div>
         <div class="flex flex-wrap">
           <label class="customLabel" for="packageStar">套票星級</label>
-          <input class="customInput" type="number" id="packageStar" />
+          <input class="customInput" type="number" id="packageStar" v-model="userData.tickStar" />
           <!-- <p class="w-full">我是警告標語</p> -->
         </div>
         <div class="flex flex-wrap">
           <label class="customLabelText" for="packageDescription">套票描述</label>
-          <textarea class="customText" id="packageDescription"></textarea>
+          <textarea
+            class="customText"
+            id="packageDescription"
+            v-model="userData.tickDescript"
+          ></textarea>
           <!-- <p class="w-full">我是警告標語</p> -->
         </div>
-        <button class="formButton">新增套票</button>
+        <button class="formButton" @click.prevent="addTicket">新增套票</button>
       </form>
     </div>
   </TheLayout>
